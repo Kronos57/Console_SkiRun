e60981f7b834dab55bd2fb7422d0c9d075cc609a
Manuel Rojas <manuelrojasb57@gmail.com>
2021-05-05 01:23:59 -0500

    Add project files.

---
+++ SkiRun/Transversal/Route.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkiRun
+{
+    class Route
+    {
+        public int MaxValue { get; set; }
+
+        public int[] BestRoute { get; set; }
+
+
+
+        public Route()
+        {
+
+        }
+
+        private void CreateMatrix() {
+
+        }
+    }
+}
