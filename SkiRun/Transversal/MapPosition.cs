6bf90cd342acbf038ba818b523ce3ee09bf5e5cd
Manuel Rojas <manuelrojasb57@gmail.com>
2021-05-06 05:53:12 -0500

    Se agregan clases de objetos transversales.

---
+++ SkiRun/Transversal/MapPosition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkiRun.Transversal
+{
+    public class MapPosition
+    {
+        public int Value { get; set; }
+
+        public int RowPosition { get; set; }
+
+        public int ColumnPosition { get; set; }
+
+        public MapPosition()
+        {
+
+        }
+
+        public MapPosition(int value, int rowPosition, int columnPosition)
+        {
+            this.Value = value;
+            this.RowPosition = rowPosition;
+            this.ColumnPosition = columnPosition;
+        }
+    }
+}
---
+++ SkiRun/Transversal/MapPositionAroundValues.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkiRun.Transversal
+{
+    public class MapPositionAroundValues
+    {
+        public MapPosition Value { get; set; }
+
+        public MapPosition UpValue { get; set; }
+
+        public MapPosition DownValue { get; set; }
+
+        public MapPosition Leftvalue { get; set; }
+
+        public MapPosition RightValue { get; set; }
+
+        public MapPositionAroundValues()
+        {
+
+        }
+    }
+}
